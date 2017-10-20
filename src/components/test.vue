05c6e1a61dadf59c64722d4f1bab1dcdfcdc8157
zhangbing <zhangbing@kaike.la>
2017-10-20 14:34:43 +0800

    tj

---
+++ src/components/test.vue
@@ -0,0 +1,31 @@
+<template>
+  <div >
+浏览器：{{type}}；<br> 版本：{{version}}
+  </div>
+</template>
+
+<script>
+var UAParser = require('ua-parser-js')
+var parser = new UAParser()
+var result = parser.getResult()
+console.log(result.browser)
+var type = result.browser.name
+var version = result.browser.version
+export default {
+  name: 'qq',
+  data () {
+    return {
+      type: type,
+      version: version
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#app{
+padding-top:70px;
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: compLoad('HelloWorld')
+      name: 'test',
+      component: compLoad('test')
     },
     {
       path: '/index',
