643b9c925a16d5ab90294e96240a7329e3297a89
zhangbing <zhangbing@kaike.la>
2017-10-21 12:06:20 +0800

    测试组件的部分

--- src/components/tologin.vue
+++ src/components/tologin.vue
@@ -1,19 +1,48 @@
 <template>
   <div >
     <ul>
-      
+       <group>
+    <x-input title="手机号码" is-type=china-mobile  placeholder=请输入11位手机号码></x-input>
+    <x-input title="验证码" class="weui-vcode" >
+        <x-button slot="right" type="primary" mini @click.native="sendcode">{{text}}</x-button>
+      </x-input>
+  </group>
     </ul>
   </div>
 </template>
 
 <script>
+import {Group,XInput,XButton, Cell} from "vux"
 export default {
   name: 'tologin',
+  components:{Group,XButton,
+    XInput,
+    Cell},
   data () {
     return {
-    
+        text:'发送验证码',
+        time:10,
+
+    }},
+    methods: {
+      timer:function(){
+            let timetemp=0
+           if(this.time>0){
+             timetemp=this.time--
+             this.text= `倒计时${timetemp}s`
+           }else{
+              this.time=10
+              clearInterval(this.t);
+              this.text= `发送验证码`
+           }
+           
+      },
+      sendcode:function(){
+           this.t=setInterval(this.timer,1000);
+      },
+
     }
-  }
+  
 }
 </script>
 
