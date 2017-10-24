383b115707f4479bd6661910f660ff863957e6c3
zhangbing <zhangbing@kaike.la>
2017-10-24 16:31:01 +0800

    tj

--- src/util/api.js
+++ src/util/api.js
@@ -7,12 +7,20 @@ const queryParser = function (url, query) {
   return `${PROXY_PATH}${url}?${qs.stringify(query)}`
 }
 
+const checkStatus = function (res) {
+  if (res.status === 200) {
+    return res
+  } else {
+    return res.statusText
+  }
+}
+
 export default {
   get (url, params) {
-    return axios.get(queryParser(url, params))
+    return axios.get(queryParser(url, params)).then(res => checkStatus(res)).then(res => res.data)
   },
   post (url, params) {
-    return axios.post(`${PROXY_PATH}${url}`, params)
+    return axios.post(`${PROXY_PATH}${url}`, params).then(res => checkStatus(res)).then(res => res.data)
   }
 
 }
